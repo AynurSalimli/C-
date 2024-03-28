58d145c0e4177a3abf53e3298daf3f74be162908
AynurSalimli <aynur.selimli@ascca.edu.az>
2024-03-29 01:57:02 +0400

    begin

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello Aynur");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code");
